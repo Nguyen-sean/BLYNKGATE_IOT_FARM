21a2065d3eafe0b53985c282df18c7acdf5ba7e0
Nguyen-sean <cbl.nguyenson@gmail.com>
2024-10-22 16:54:32 +0700

    Lesson 1

--- examples/IOT_FARM_LESSON_1/IOT_FARM_LESSON_1.ino
+++ examples/IOT_FARM_LESSON_1/IOT_FARM_LESSON_1.ino
@@ -1,105 +1,82 @@
 /*
-  Title:  LDR and Buzzer demo
-  Author: Daniel Hoang
-  Date:   5/6/2024
-  Description: Read value of the LDR sensor and write value into Blynk every 5s.
-              - Virtual Pin V2 (Chart) on Blynk for LDR sensor
+  Tiêu đề:  Demo Nhiệt độ và Độ ẩm
+  Mô tả: Đọc giá trị từ cảm biến DHT11/22 và gửi lên Blynk mỗi 5 giây.
+              - Virtual Pin V1 (cho nhiệt độ)
+              - Virtual Pin V2 (cho độ ẩm)
 */
-// Step 1: Get this infomation from Blynk.cloud
-#define BLYNK_TEMPLATE_ID "Copy_BLYNK_TEMPLATE_ID_From_BlynkCloud"
-#define BLYNK_TEMPLATE_NAME "Copy_BLYNK_TEMPLATE_NAME_From_BlynkCloud"
-#define BLYNK_AUTH_TOKEN "Copy_BLYNK_AUTH_TOKEN_From_BlynkCloud"
 
-#define LDR_MIN_VALUE 0
-#define LDR_MAX_VALUE 692
+// Cấu hình Blynk
+#define BLYNK_TEMPLATE_ID "TMPL6Gyrmi-8_"
+#define BLYNK_TEMPLATE_NAME "blynk gate"
+#define BLYNK_AUTH_TOKEN "fJltZnwukHtoRz0IctDps7mbZgAod49K"
 
-#define LDR_PIN A1
+// Định nghĩa Virtual Pin cho nhiệt độ và độ ẩm
+#define VIRTUAL_PIN_TEMP 1 // Virtual Pin V1 dành cho nhiệt độ
+#define VIRTUAL_PIN_HUMIDITY 2 // Virtual Pin V2 dành cho độ ẩm
 
-
-// Step 2: include library
 #include "BlynkGate.h"
-#include "MKL_LiquidCrystal_I2C.h"
-// Step 3: Setup WiFi
-char auth[] = BLYNK_AUTH_TOKEN;
-char ssid[] = "MakerLab.vn";  // Key in your wifi name (Bandwidth 2.4Ghz). You can check with your smart phone for your wifi name
-char pass[] = "";             // Key in your wifi password.
+#include "MKL_DHT.h"
 
-unsigned long intervalLCD = 0;
-MKL_LiquidCrystal_I2C lcd(0x27, 16, 2);
+// Thiết lập chân cho DHT
+#define DHTPIN A1 // Chân dữ liệu từ cảm biến DHT
+MKL_DHT dht(DHTPIN, DHT11);
 
-unsigned long lastTimeSend = 0;
+char ssid[] = "Hshop.vn";
+char pass[] = "";
 
-int valueLDR = 0;
-int mapValue = 0;
-int percent = 0;
+// Biến để sử dụng với millis()
+// Variables for millis timing
+unsigned long previousMillis = 0;
+const long interval = 5000; // Khoảng thời gian chờ (5 giây)
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println(F("Start BlynkGate I2C"));
-  lcd.init();
-  lcd.backlight();
-  // Step 4: begin BlynkGate
-  Blynk.begin(auth, ssid, pass);
+  dht.begin();
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   Blynk.run();
-  // DO NOT using delay
-  readLDRSensor();
-  if (millis() - intervalLCD > 500) {
-    showOnLCD();
-    intervalLCD = millis();
-  }
-  // Try using millis() and use "Blynk.virtualWrite" at least 10s at a time to avoid spamming the server
-  if (millis() - lastTimeSend >= 10000) {
-    lastTimeSend = millis();
-    Blynk.virtualWrite(2, percent);
-    // Step 6: Send Virtual pin Value
+
+  // Lấy thời gian hiện tại
+  // Get the current time
+  unsigned long currentMillis = millis();
+
+  // So sánh thời gian đã trôi qua với khoảng thời gian mong muốn
+  // Compare the time passed with the interval
+  if (currentMillis - previousMillis >= interval) {
+    // Cập nhật thời gian trước đó
+    // Update the previous time
+    previousMillis = currentMillis;
+
+    // Gọi hàm gửi dữ liệu cảm biến
+    // Call the function to send sensor data
+    sendSensorData();
   }
 }
 
-// Step 5: Get Virtual pin Value
+void sendSensorData() {
+  float temp = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  
+  // Gửi lên Blynk
+  Blynk.virtualWrite(VIRTUAL_PIN_TEMP, temp); // gửi nhiệt độ lên Blynk
+  Blynk.virtualWrite(VIRTUAL_PIN_HUMIDITY, humidity); // gửi độ ẩm lên Blynk
+
+  Serial.print("Nhiệt độ: ");
+  Serial.print(temp);
+  Serial.print(" *C, Độ ẩm: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+}
+
+// Cấu trúc BLYNK_WRITE_DEFAULT để nhận dữ liệu từ tất cả các virtual pin
 BLYNK_WRITE_DEFAULT() {
-  // double myDouble = param.asFloat();
-  // String myString = param.asString();
-  int myInt = param.asInt();
-
+  // Nhận giá trị từ ứng dụng Blynk (int, float, string, etc.)
+  int myInt = param.asInt();  // Đọc giá trị dạng int, có thể dùng asFloat(), asString() cho các kiểu khác
+  
   Serial.print("input V");
-  Serial.print(request.pin);
+  Serial.print(request.pin); // In ra số pin ảo đang nhận dữ liệu
   Serial.print(": ");
-  Serial.println(myInt);
-}
-
-void readLDRSensor() {
-  valueLDR = analogRead(LDR_PIN);
-  mapValue = map(valueLDR, LDR_MAX_VALUE, LDR_MIN_VALUE, 0, 255);
-  mapValue = constrain(mapValue, 0, 255);
-  percent = map(mapValue, 0, 255, 0, 100);
-}
-
-void showOnLCD() {
-  lcd.setCursor(1, 0);
-  lcd.print("IoT StarterKit");
-  lcd.setCursor(0, 1);
-  lcd.print("Value: ");
-  lcd.setCursor(7, 1);
-  if (valueLDR / 10 == 0) {             // valueLDR la so co 1 chu so
-    lcd.print(valueLDR);
-    lcd.print("  ");
-  } else if (valueLDR / 100 == 0) {     // valueLDR la so co 2 chu so
-    lcd.print(valueLDR); 
-    lcd.print(" ");
-  } else {                              // valueLDR la so co 3 chu so
-    lcd.print(valueLDR);
-  }
-  lcd.setCursor(12, 1);
-  if (percent < 10) {
-    lcd.print("  ");
-  } else if (percent < 100) {
-    lcd.print(" ");
-  }
-  lcd.print(percent);
-  lcd.print("%");
+  Serial.println(myInt);     // In ra giá trị nhận được
 }
